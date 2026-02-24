47eef8b16ae284330e970d50c2b28754ec232324
DmaxB <135641740+DmaxB13@users.noreply.github.com>
2026-02-24 16:16:26 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,18 @@
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore-compat.js"></script>
+// Firebase Core
+document.write('<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></' + 'script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-auth-compat.js"></' + 'script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore-compat.js"></' + 'script>');
 
-<script>
-const firebaseConfig = {
-  apiKey: "AIzaSyBMg2ffCPVmhI58kvOI3gAenyJHWRc9wME",
-  authDomain: "isite-2-test.firebaseapp.com",
-  projectId: "isite-2-test",
-  storageBucket: "isite-2-test.firebasestorage.app",
-  messagingSenderId: "333012577798",
-  appId: "1:333012577798:web:0141f6e35a0f9e48eb9d87"
-};
+// Initialize Firebase
+document.addEventListener("DOMContentLoaded", () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBMg2ffCPVmhI58kvOI3gAenyJHWRc9wME",
+        authDomain: "isite-2-test.firebaseapp.com",
+        projectId: "isite-2-test",
+        storageBucket: "isite-2-test.firebasestorage.app",
+        messagingSenderId: "333012577798",
+        appId: "1:333012577798:web:0141f6e35a0f9e48eb9d87"
+    };
 
-firebase.initializeApp(firebaseConfig);
-</script>
+    firebase.initializeApp(firebaseConfig);
+});
