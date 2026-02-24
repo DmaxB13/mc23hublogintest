f5ebac82ab4b95ab3cd3baa8c9f98fdfc9ffd642
DmaxB <135641740+DmaxB13@users.noreply.github.com>
2026-02-24 16:10:21 +0100

    Add Firebase configuration and initialization script

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBMg2ffCPVmhI58kvOI3gAenyJHWRc9wME",
+  authDomain: "isite-2-test.firebaseapp.com",
+  projectId: "isite-2-test",
+  storageBucket: "isite-2-test.firebasestorage.app",
+  messagingSenderId: "333012577798",
+  appId: "1:333012577798:web:0141f6e35a0f9e48eb9d87"
+};
+
+firebase.initializeApp(firebaseConfig);
+</script>
